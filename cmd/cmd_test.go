5024cc9291cbd2be95ea49f3c964e8405e31c34f
hero0926 <esistkalt@naver.com>
2019-10-19 11:22:53 +0200

    add open repos and filter test

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -2,3 +2,43 @@
 // Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 package cmd
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOpenReposAndFilter(t *testing.T) {
+
+	git_repo := "../.git"
+	not_git_repo := "./hello"
+	not_even_dir := "lol"
+
+	repoList_work := Repolist{
+		&git_repo,
+	}
+
+	repoList_not_repo := Repolist{
+		&not_git_repo,
+	}
+
+	repoList_not_url := Repolist{
+		&not_even_dir,
+	}
+
+	fr, er := OpenReposAndFilter(repoList_work)
+	assert.Equal(t, 0, len(er))
+	assert.EqualValues(t, repoList_work, fr)
+
+	errorString := errors.New("repository does not exist")
+	fr, er = OpenReposAndFilter(repoList_not_repo)
+	assert.Equal(t, 1, len(er))
+	assert.EqualValues(t, errorString, er[0].e)
+
+	fr, er = OpenReposAndFilter(repoList_not_url)
+	assert.Equal(t, 1, len(er))
+	assert.EqualValues(t, errorString, er[0].e)
+
+}
